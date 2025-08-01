24735468d5200455f7969a86e5febf89826c42ac
Juan Guevara M. <juanexgue@gmail.com>
2025-07-31 23:00:49 -0500

    init commit

---
+++ src/main/java/com/juanex/productofinanciero_service_challengebackend/ProductofinancieroServiceChallengebackendApplication.java
@@ -0,0 +1,13 @@
+package com.juanex.productofinanciero_service_challengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductofinancieroServiceChallengebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductofinancieroServiceChallengebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanex/productofinanciero_service_challengebackend/ProductofinancieroServiceChallengebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanex.productofinanciero_service_challengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductofinancieroServiceChallengebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
